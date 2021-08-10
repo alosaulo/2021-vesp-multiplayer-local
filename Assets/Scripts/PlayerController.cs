5fee4e8382c13118e805cf2a7067d4da1cfc4f71
Saulo Caliman <saulocalimangomes@gmail.com>
2021-08-10 17:06:44 -0300

    Adição de animação de ataques

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,91 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
 
+    public float atkDelay;
+    
+    public float countAtkDelay;
+
     Rigidbody2D rigidbody2D;
+
+    Animator animator;
+
+    float lastX, lastY;
+    float hAxis, vAxis;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxisRaw("HorizontalP"+playerNumber);
-        float vAxis = Input.GetAxisRaw("VerticalP"+playerNumber);
+        hAxis = Input.GetAxis("HorizontalP" + playerNumber);
+        vAxis = Input.GetAxis("VerticalP" + playerNumber);
 
-        Vector2 newPos = new Vector2(hAxis, vAxis);
+        if (Mathf.Abs(hAxis) >= 0.000000001) {
+            lastX = hAxis;
+            lastY = 0;
+        }
+        if (Mathf.Abs(vAxis) >= 0.000000001)
+        {
+            lastY = vAxis;
+            lastX = 0;
+        }
 
-        rigidbody2D.velocity = newPos.normalized * speed;
+        if (countAtkDelay > atkDelay)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                countAtkDelay = 0;
+                if (lastX > 0)
+                {
+                    PlayAnimation("atk-right");
+                }
+                else if (lastX < 0)
+                {
+                    PlayAnimation("atk-left");
+                }
+                else if (lastY > 0)
+                {
+                    PlayAnimation("atk-up");
+                }
+                else if (lastY < 0)
+                {
+                    PlayAnimation("atk-down");
+                }
+            }
+        }
+        else {
+            countAtkDelay += Time.fixedDeltaTime;
+        }
+
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 newPos = new Vector2(hAxis, vAxis).normalized * speed * Time.fixedDeltaTime;
+
+        rigidbody2D.velocity = newPos;
+        
+        animator.SetFloat("Y", lastY);
+
+        animator.SetFloat("X", lastX);
+
+    }
+
+    string currentAnimation;
+    void PlayAnimation(string animation) {
+        if (currentAnimation == animation)
+            return;
+        currentAnimation = animation;
+        animator.Play(currentAnimation);
+    }
+
+    public void PlayMovimentoAnimation() {
+        PlayAnimation("Movimento");
+    }
+
 }
