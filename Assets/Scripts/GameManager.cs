31774189490fe50c1ff3809f2c65cfc41b1504b5
Saulo Caliman <saulocalimangomes@gmail.com>
2021-09-21 16:57:00 -0300

    Adição de camera que segue os players pelo cinemachine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,17 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 
+    public float 
+        yMax = float.MinValue, 
+        yMin = float.MaxValue, 
+        xMax = float.MinValue, 
+        xMin = float.MaxValue,
+        totalX = 0, totalY = 0;
+
+    public Cinemachine.CinemachineVirtualCamera cvCam4All;
+
+    public Transform[] playersPositions;
+
     public Image[] playersLifeImg;
 
     // Start is called before the first frame update
@@ -17,11 +28,58 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetPlayerDistance();
     }
 
     public Image GetLifeImage(int playerNumber) {
         return playersLifeImg[playerNumber - 1];
+    }
+
+    Vector2 pos;
+    private void OnDrawGizmos()
+    {
+        pos = new Vector2(totalX, totalY);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(pos,0.3f);
+    }
+
+    void GetPlayerDistance() {
+        
+        yMax = float.MinValue;
+        yMin = float.MaxValue;
+        xMax = float.MinValue;
+        xMin = float.MaxValue;
+
+        for (int i = 0; i < playersPositions.Length; i++)
+        {
+            Vector3 playerPos = playersPositions[i].position;
+            if (yMax < playerPos.y)
+            {
+                yMax = playerPos.y;
+            }
+            if (yMin > playerPos.y)
+            {
+                yMin = playerPos.y;
+            }
+            if (xMax < playerPos.x)
+            {
+                xMax = playerPos.x;
+            }
+            if (xMin > playerPos.x)
+            {
+                xMin = playerPos.x;
+            }
+        }
+
+        totalX = (xMax + xMin) / 2;
+        totalY = (yMin + yMax) / 2;
+
+        cvCam4All.transform.position = new Vector3(totalX, totalY, -10f);
+
+        yMax = float.MinValue;
+        yMin = float.MaxValue;
+        xMax = float.MinValue;
+        xMin = float.MaxValue;
     }
 
 }
